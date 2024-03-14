369a9aa29a7954727d4149da7a9adeb09056c5b6
praveen <napraveen03@gmail.com>
2024-03-14 12:17:16 +0530

    changed

--- client/src/components/MovieDetail.js
+++ client/src/components/MovieDetail.js
@@ -1,7 +1,10 @@
+import { Button, ratingClasses } from "@mui/material";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 
 const MovieDetail = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const [movie, setMovie] = useState([]);
   useEffect(() => {
@@ -11,12 +14,16 @@ const MovieDetail = () => {
       .then((data) => data.json())
       .then((mv) => setMovie(mv));
   }, []);
+
+  const ratingStyles = {
+    color: movie.rating >= 8.5 ? "green" : "red",
+  };
   console.log("akjlgbarsjbg", movie);
   return (
     <div>
       <iframe
-        width="100%"
-        height="900px"
+        width="70%"
+        height="700px"
         src={movie.trailer}
         title={movie.name}
         frameborder="0"
@@ -26,8 +33,19 @@ const MovieDetail = () => {
       <div className="movie-detail-container">
         <div className="movie-spec">
           <h2 className="movie-name">{movie.name}</h2>
+          <h3 className="movie-rating" style={ratingStyles}>
+            🌟 {movie.rating}
+          </h3>
         </div>
+        <p className="movie-summary">{movie.summary}</p>
       </div>
+      <Button
+        variant="contained"
+        startIcon={<ArrowBackIosIcon />}
+        onClick={() => navigate(-1)}
+      >
+        Back
+      </Button>
     </div>
   );
 };
